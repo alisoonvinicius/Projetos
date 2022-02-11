2046031244e758b1e6999c7bcf0278f1a41a1890
alisoonvinicius <alisoonv12@gmail.com>
2022-02-10 22:38:42 -0300

    primeiro commit

---
+++ components/Menu.js
@@ -0,0 +1,11 @@
+import link from "next/link";
+
+function menu() {
+    <div>
+        <link href="/">
+            <a>Home</a>
+        </link>
+    
+    </div>
+};
+export default menu;
---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Menu from '../components/Menu';
+function Home(){
+    return (
+    <div>
+        <head>
+            <meta charSet='utf-8' />
+            <meta name='robots' content='index,folow'/>
+            <meta name='description' content='Site sobre carros usados'/>
+            <meta name='author' content='Alison'/>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <title>Wikihow</title>
+        </head>
+        
+        <menu />
+
+     conteúdo do topo do site!
+                
+    </div>)
+}
+
+export default Home;
